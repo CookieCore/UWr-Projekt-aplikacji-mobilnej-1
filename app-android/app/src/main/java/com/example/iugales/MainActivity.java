17b3a8295ea308455911984802fa62f4acd8f717
Marcin <marcinu456@gmail.com>
2021-03-26 07:14:26 +0100

    Auto stash before merge of "Production" and "origin/Production"

--- app-android/app/src/main/java/com/example/iugales/MainActivity.java
+++ app-android/app/src/main/java/com/example/iugales/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.iugales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.iugales.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.debugToLoginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
