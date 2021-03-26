78c17570f2893578e71f6908c709af85723168a0
Marcin <marcinu456@gmail.com>
2021-03-26 07:21:16 +0100

    Add login debug impl

--- app-android/app/src/main/java/com/example/iugales/MainActivity.java
+++ app-android/app/src/main/java/com/example/iugales/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.iugales.databinding.ActivityMainBinding;
 
@@ -20,9 +21,17 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.debugToLoginBtn.setOnClickListener(v -> {
+        binding.debugGoLogin.setOnClickListener(v -> {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
+        });
+
+        binding.debugGoHomePageUsr.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),"Not Implement Yet", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.debugGoHomePageManager.setOnClickListener(v -> {
+            Toast.makeText(getApplicationContext(),"Not Implement Yet", Toast.LENGTH_SHORT).show();
         });
     }
 }
