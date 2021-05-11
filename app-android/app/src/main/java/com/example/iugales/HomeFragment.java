9df91f4362e103d0fb6e20b83de302c57b3d20f3
SebaK <sebastian.k1999@gmail.com>
2021-05-11 19:05:15 +0200

    confict fixed, basics of home layout

--- app-android/app/src/main/java/com/example/iugales/HomeFragment.java
+++ app-android/app/src/main/java/com/example/iugales/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.iugales;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +8,33 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.iugales.util.Util;
+import com.example.iugales.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    private FragmentHomeBinding mBinding;
+    
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        mBinding = FragmentHomeBinding.inflate(getLayoutInflater());
+        View v = mBinding.getRoot();
+
+        mBinding.userSkillsT0.setVisibility(View.VISIBLE);
+        mBinding.userSkillsI0.setVisibility(View.VISIBLE);
+        mBinding.userSkillsT0.setText(R.string.skill_cpp);
+
+        mBinding.userSkillsT1.setVisibility(View.VISIBLE);
+        mBinding.userSkillsI1.setVisibility(View.VISIBLE);
+        mBinding.userSkillsT1.setText(R.string.skill_assembly);
+
+        mBinding.userSkillsT2.setVisibility(View.VISIBLE);
+        mBinding.userSkillsI2.setVisibility(View.VISIBLE);
+        mBinding.userSkillsT2.setText(R.string.skill_white_space);
+
+        return v;
     }
 }
