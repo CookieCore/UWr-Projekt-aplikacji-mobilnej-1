926d38ec08da222a8ff99f611caf2562d17a9779
Krzysiek (Chris) <kukizk@gmail.com>
2021-05-19 03:02:55 +0200

    homePage skills and lvl + hello firstLastName
    znowu bo bledy

--- app-android/app/src/main/java/com/example/iugales/HomeFragment.java
+++ app-android/app/src/main/java/com/example/iugales/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.iugales;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.iugales.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding mBinding;
     private CalendarAdapter mAdapterToday;
     private CalendarAdapter mAdapterTomorrow;
     private CalendarAdapter mAdapterLater;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    private String TAG = "HomeFragment";
 
     public HomeFragment() {
         // Required empty public constructor
@@ -30,17 +48,69 @@ public class HomeFragment extends Fragment {
         mBinding = FragmentHomeBinding.inflate(getLayoutInflater());
         View v = mBinding.getRoot();
 
+        // firebase data
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser curUsr = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        DocumentReference dbUser = db.collection("Users").document(curUsr.getUid());
+
+        // first & last name
+        dbUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String firstLastName = document.getData().get("firstName").toString() +
+                                " " + document.getData().get("lastName").toString();
+                        mBinding.userUsername.setText(firstLastName);
+                    }
+                }
+            }
+        });
+
+        // skills
+        dbUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Long> skills;
+                        skills = (Map<String, Long>) document.getData().get("Skills");
+                        Log.d(TAG, "onComplete: " + skills);
+                        // sort map in reverse
+                        skills = skills.entrySet()
+                                .stream()
+                                .sorted((Map.Entry.<String, Long>comparingByValue().reversed()))
+                                .collect(
+                                        Collectors.toMap(
+                                                Map.Entry::getKey,
+                                                Map.Entry::getValue,
+                                                (e1, e2) -> e1, LinkedHashMap::new
+                                        )
+                                );
+                        Log.d(TAG, "onComplete: " + skills);
+                        while (skills.size() < 3) {
+                            skills.put("Null", 0l);
+                        }
+                        Iterator<Map.Entry<String, Long>> myIterator = skills.entrySet().iterator();
+                        Map.Entry<String, Long> skill = myIterator.next();
+                        mBinding.userSkillsT0.setText(skill.getKey() + ": " + skill.getValue() + "/4");
+                        skill = myIterator.next();
+                        mBinding.userSkillsT1.setText(skill.getKey() + ": " + skill.getValue() + "/4");
+                        skill = myIterator.next();
+                        mBinding.userSkillsT2.setText(skill.getKey() + ": " + skill.getValue() + "/4");
+                    }
+                }
+            }
+        });
         mBinding.userSkillsT0.setVisibility(View.VISIBLE);
         mBinding.userSkillsI0.setVisibility(View.VISIBLE);
-        mBinding.userSkillsT0.setText(R.string.skill_cpp);
-
         mBinding.userSkillsT1.setVisibility(View.VISIBLE);
         mBinding.userSkillsI1.setVisibility(View.VISIBLE);
-        mBinding.userSkillsT1.setText(R.string.skill_assembly);
-
         mBinding.userSkillsT2.setVisibility(View.VISIBLE);
         mBinding.userSkillsI2.setVisibility(View.VISIBLE);
-        mBinding.userSkillsT2.setText(R.string.skill_white_space);
 
         String namesToday[] = {"today 1","today 2"};
         String datesToday[] = {"d1","d2"};
