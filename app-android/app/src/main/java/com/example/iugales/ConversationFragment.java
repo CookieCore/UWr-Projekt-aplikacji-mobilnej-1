993d80ff04653cd54cf3586a56133fc92cd2f152
Krzysiek (Chris) <kukizk@gmail.com>
2021-06-16 22:50:10 +0200

    pobiera czesc danych do czatu (zakomentowane)

--- app-android/app/src/main/java/com/example/iugales/ConversationFragment.java
+++ app-android/app/src/main/java/com/example/iugales/ConversationFragment.java
@@ -5,15 +5,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.iugales.databinding.FragmentConversationBinding;
+import com.example.iugales.model.ChatBubble;
+import com.example.iugales.model.ChatListItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.type.DateTime;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
 
 public class ConversationFragment extends Fragment {
     private FragmentConversationBinding mBinding;
     private MessagesAdapter mAdapter;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser curUsr;
+    private FirebaseFirestore db;
+
+    private String TAG = "ConversationFragment";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +55,40 @@ public class ConversationFragment extends Fragment {
         mBinding.msgContainer.setLayoutManager( linearLayout );
         mAdapter = new MessagesAdapter(this);
         mBinding.msgContainer.setAdapter(mAdapter);
+
+        String chatId = "oB0BDLGGuB9MdzlzdlOa"; // TODO: 6/16/21 give me chat id from clicket chat form list
+
+        // download chats
+        mAuth = FirebaseAuth.getInstance();
+        curUsr = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+
+        // get chat reference
+        DocumentReference dbChat = db.collection("Chat").document(chatId);
+        DocumentReference dbCurUser = db.collection("Users").document(curUsr.getUid());
+
+        // chat items sorted
+//        dbChat.collection("Chat")
+//                .orderBy("DateSend", Query.Direction.DESCENDING)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        ArrayList<ChatBubble> chatBubbles = new ArrayList<ChatBubble>();
+//                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            Map<String, Object> myData = document.getData();
+//
+//                            chatBubbles.add(new ChatBubble(
+//                                document.getId(),
+//                                (String) myData.get("value"),
+//                                (Date) myData.get("DateSend"),
+//                                "",
+//                                "",
+//                                true
+//                            ));
+//                        }
+//                    }
+//                });
 
         return v;
     }
